b99ac1f9db7f485b4ed1a6ba45b2e3df7e6eab39
John Blum <john.blum@earnin.com>
2025-03-13 19:38:26 -0700

    Enable 'kotlin-dsl' in buildSrc/.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,9 @@
+// @see https://docs.gradle.org/current/userguide/implementing_gradle_plugins_precompiled.html
+// @see https://docs.gradle.org/current/samples/sample_convention_plugins.html#compiling_convention_plugins
 plugins {
     id("java-gradle-plugin")
     kotlin("jvm") version "2.1.10"
+    `kotlin-dsl`
 }
 
 repositories {
